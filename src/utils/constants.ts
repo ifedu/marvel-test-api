24ac5dd66816c3f2ab21e7a94ba64c51e4bf3929
Edu Pérez-Bermejo <ifedu@outlook.com>
2024-03-06 08:52:20 +0100

    fix: use .env variable correctly

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const MARVEL_API_BASE_URL = "https://gateway.marvel.com/v1/public";
-export const MARVEL_API_KEY = process.env.MARVEL_API_KEY;
+export const MARVEL_API_KEY = process.env.NEXT_PUBLIC_MARVEL_API_KEY;
